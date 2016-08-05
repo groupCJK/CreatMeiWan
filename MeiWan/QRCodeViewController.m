227764b84b8c9d771b7f7af1dc72b3fa16e43f5e
Chutanyuan <17839302862@163.com>
2016-08-05 11:56:14 +0800

    二维码保存到相册
    yes

--- MeiWan/QRCodeViewController.m
+++ MeiWan/QRCodeViewController.m
@@ -9,12 +9,21 @@
 #import "QRCodeViewController.h"
 #import "QRCodeGenerator.h"
 #import "scanViewController.h"
-@interface QRCodeViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+
+#import <AssetsLibrary/ALAsset.h>
+
+#import <AssetsLibrary/ALAssetsLibrary.h>
+
+#import <AssetsLibrary/ALAssetsGroup.h>
+
+#import <AssetsLibrary/ALAssetRepresentation.h>
+#import "ShowMessage.h"
+@interface QRCodeViewController ()
 {
     AVCaptureSession * session;//输入输出的中间桥梁
 }
 @property(nonatomic) CGRect rectOfInterest;
-
+@property(nonatomic,assign) UIImage * image;
 @end
 
 @implementation QRCodeViewController
@@ -24,11 +33,17 @@
     UIImageView * imageView = [[UIImageView alloc]init];
     imageView.center = self.view.center;
     imageView.bounds = CGRectMake(0, 0, 300, 300);
+    imageView.userInteractionEnabled = YES;
     //公会会长名称
     NSString * string = @"madezhizhang";
     UIImage*tempImage=[QRCodeGenerator qrImageForString:string imageSize:300 Topimg:[UIImage imageNamed:@"gonghui"]];
     imageView.image=tempImage;
     [self.view addSubview:imageView];
+    //长按手势
+    UILongPressGestureRecognizer * longPressGesture = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPressGesture:)];
+    longPressGesture.minimumPressDuration = 2.0;
+    [imageView addGestureRecognizer:longPressGesture];
+    
 
     // Do any additional setup after loading the view.
     UIButton * saoScan = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -36,7 +51,57 @@
     saoScan.backgroundColor = [UIColor greenColor];
     [saoScan addTarget:self action:@selector(saoScanClick) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:saoScan];
+    
+//    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+//    [library addAssetsGroupAlbumWithName:@"美玩" resultBlock:^(ALAssetsGroup *group) {
+//        
+//        //创建相簿成功
+//        
+//    } failureBlock:^(NSError *error) {
+//        //失败
+//    }];
+    
+    
 }
+- (void)longPressGesture:(UILongPressGestureRecognizer *)gesture
+{
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        
+        UIImageView * imageview = (UIImageView *)[gesture view];
+    
+        [self showMessageAlert:@"保存图片" image:imageview.image];
+        
+    }else {
+
+    }
+
+}
+//- (void)saveImageToPhotos:(UIImage*)savedImage
+//{
+//    
+//    UIImageWriteToSavedPhotosAlbum(savedImage, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+//    
+//}
+//- (void)image: (UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo
+//{
+//    if(error != NULL){
+//        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"存储失败"
+//                                                       message:@"请打开 设置-隐私-照片 来进行设置"
+//                                                      delegate:nil
+//                                             cancelButtonTitle:@"确定"
+//                                             otherButtonTitles:nil, nil];
+//        [alert show];
+//    }else{
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"图片保存成功"
+//                                                        message:nil
+//                                                       delegate:self
+//                                              cancelButtonTitle:@"确定"
+//                                              otherButtonTitles:nil];
+//        [alert show];
+//    }
+//
+//}
+/**跳转扫码页面*/
 - (void)saoScanClick
 {
     scanViewController * san = [[scanViewController alloc]init];
@@ -44,53 +109,140 @@
     [self.navigationController pushViewController:san animated:YES];
     
 //    // Do any additional setup after loading the view, typically from a nib.
-//    //获取摄像设备
-//    AVCaptureDevice * device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-//    //创建输入流
-//    AVCaptureDeviceInput * input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
-//    //创建输出流
-//    AVCaptureMetadataOutput * output = [[AVCaptureMetadataOutput alloc]init];
-//    //设置代理 在主线程里刷新
-//    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-//    [output setRectOfInterest:CGRectMake((124)/dtScreenHeight,((dtScreenWidth-220)/2)/dtScreenWidth,220/dtScreenHeight,220/dtScreenWidth)];
-//    
-//    //初始化链接对象
-//    session = [[AVCaptureSession alloc]init];
-//    //高质量采集率
-//    [session setSessionPreset:AVCaptureSessionPresetHigh];
-//    
-//    [session addInput:input];
-//    [session addOutput:output];
-//    //设置扫码支持的编码格式(如下设置条形码和二维码兼容)
-//    output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
-//    
-//    AVCaptureVideoPreviewLayer * layer = [AVCaptureVideoPreviewLayer layerWithSession:session];
-//    layer.videoGravity=AVLayerVideoGravityResizeAspectFill;
-//    layer.frame=self.view.layer.bounds;
-//    [self.view.layer insertSublayer:layer atIndex:0];
-//    //开始捕获
-//    [session startRunning];
-}
-
-
--(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
-    if (metadataObjects.count>0) {
-        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex : 0 ];
-        [session stopRunning];
-        [self showMessageAlert:metadataObject.stringValue];
-    }
 }
 /**提示框*/
-- (void)showMessageAlert:(NSString *)message
+- (void)showMessageAlert:(NSString *)message image:(UIImage *)image
 {
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:message preferredStyle:UIAlertControllerStyleActionSheet];
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action){
         
     }];
-    UIAlertAction * sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:nil];
+    UIAlertAction * sureAction = [UIAlertAction actionWithTitle:@"保存到相册" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        self.image = image;
+        [self createAlbum];
+    }];
+    
     [alertController addAction:cancelAction];
     [alertController addAction:sureAction];
     [self presentViewController:alertController animated:YES completion:nil];
+}
+
+
+
+#pragma mark - 创建相册
+- (void)createAlbum
+{
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    NSMutableArray *groups=[[NSMutableArray alloc]init];
+    ALAssetsLibraryGroupsEnumerationResultsBlock listGroupBlock = ^(ALAssetsGroup *group, BOOL *stop)
+    {
+        if (group)
+        {
+            [groups addObject:group];
+        }
+        
+        else
+        {
+            BOOL haveHDRGroup = NO;
+            
+            for (ALAssetsGroup *gp in groups)
+            {
+                NSString *name =[gp valueForProperty:ALAssetsGroupPropertyName];
+                
+                if ([name isEqualToString:@"美玩"])
+                {
+                    haveHDRGroup = YES;
+                }
+            }
+            
+            if (!haveHDRGroup)
+            {
+                [assetsLibrary addAssetsGroupAlbumWithName:@"美玩" resultBlock:^(ALAssetsGroup *group) {
+                    
+                    [groups addObject:group];
+               
+                } failureBlock:nil];
+                haveHDRGroup = YES;
+            }
+        }
+        
+    };
+    //创建相簿
+    [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAlbum usingBlock:listGroupBlock failureBlock:nil];
+    
+    [self saveToAlbumWithMetadata:nil imageData:UIImagePNGRepresentation(self.image) customAlbumName:@"美玩" completionBlock:^ {
+         [ShowMessage showMessage:@"保存成功"];
+         
+     }failureBlock:^(NSError *error){
+         //处理添加失败的方法显示alert让它回到主线程执行，不然那个框框死活不肯弹出来
+         dispatch_async(dispatch_get_main_queue(), ^{
+             
+             //添加失败一般是由用户不允许应用访问相册造成的，这边可以取出这种情况加以判断一下
+             if([error.localizedDescription rangeOfString:@"User denied access"].location != NSNotFound ||[error.localizedDescription rangeOfString:@"用户拒绝访问"].location!=NSNotFound){
+                 UIAlertView *alert=[[UIAlertView alloc]initWithTitle:error.localizedDescription message:error.localizedFailureReason delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", nil) otherButtonTitles: nil];
+                 
+                 [alert show];
+             }
+         });
+     }];
+}
+
+- (void)saveToAlbumWithMetadata:(NSDictionary *)metadata
+                      imageData:(NSData *)imageData
+                customAlbumName:(NSString *)customAlbumName
+                completionBlock:(void (^)(void))completionBlock
+                   failureBlock:(void (^)(NSError *error))failureBlock
+{
+    
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    __weak ALAssetsLibrary *weakSelf = assetsLibrary;
+    void (^AddAsset)(ALAssetsLibrary *, NSURL *) = ^(ALAssetsLibrary *assetsLibrary, NSURL *assetURL) {
+        [assetsLibrary assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+            [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                
+                if ([[group valueForProperty:ALAssetsGroupPropertyName] isEqualToString:customAlbumName]) {
+                    [group addAsset:asset];
+                    if (completionBlock) {
+                        completionBlock();
+                    }
+                }
+            } failureBlock:^(NSError *error) {
+                if (failureBlock) {
+                    failureBlock(error);
+                }
+            }];
+        } failureBlock:^(NSError *error) {
+            if (failureBlock) {
+                failureBlock(error);
+            }
+        }];
+    };
+    [assetsLibrary writeImageDataToSavedPhotosAlbum:imageData metadata:metadata completionBlock:^(NSURL *assetURL, NSError *error) {
+        if (customAlbumName) {
+            [assetsLibrary addAssetsGroupAlbumWithName:customAlbumName resultBlock:^(ALAssetsGroup *group) {
+                if (group) {
+                    [weakSelf assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+                        [group addAsset:asset];
+                        if (completionBlock) {
+                            completionBlock();
+                        }
+                    } failureBlock:^(NSError *error) {
+                        if (failureBlock) {
+                            failureBlock(error);
+                        }
+                    }];
+                } else {
+                    AddAsset(weakSelf, assetURL);
+                }
+            } failureBlock:^(NSError *error) {
+                AddAsset(weakSelf, assetURL);
+            }];
+        } else {
+            if (completionBlock) {
+                completionBlock();
+            }
+        }
+    }];
 }
 
 @end
--- MeiWan/scanViewController.m
+++ MeiWan/scanViewController.m
@@ -8,7 +8,10 @@
 
 #import "scanViewController.h"
 #import "CorlorTransform.h"
-@interface scanViewController ()
+@interface scanViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+{
+    AVCaptureSession * session;//输入输出的中间桥梁
+}
 
 @end
 
@@ -68,6 +71,51 @@
     } completion:nil];
 
 
+    
+    //获取摄像设备
+    AVCaptureDevice * device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    //创建输入流
+    AVCaptureDeviceInput * input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+    //创建输出流
+    AVCaptureMetadataOutput * output = [[AVCaptureMetadataOutput alloc]init];
+    //设置代理 在主线程里刷新
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [output setRectOfInterest:CGRectMake((124)/dtScreenHeight,((dtScreenWidth-220)/2)/dtScreenWidth,220/dtScreenHeight,220/dtScreenWidth)];
+    //初始化链接对象
+    session = [[AVCaptureSession alloc]init];
+    //高质量采集率
+    [session setSessionPreset:AVCaptureSessionPresetHigh];
+    [session addInput:input];
+    [session addOutput:output];
+    //设置扫码支持的编码格式(如下设置条形码和二维码兼容)
+    output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+    
+    AVCaptureVideoPreviewLayer * layer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    layer.videoGravity=AVLayerVideoGravityResizeAspectFill;
+    layer.frame=self.view.layer.bounds;
+    [self.view.layer insertSublayer:layer atIndex:0];
+    //开始捕获
+    [session startRunning];
+
+}
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    if (metadataObjects.count>0) {
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex : 0 ];
+        [session stopRunning];
+        [self showMessageAlert:metadataObject.stringValue];
+    }
+}
+/**提示框*/
+- (void)showMessageAlert:(NSString *)message
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action){
+        
+    }];
+    UIAlertAction * sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:nil];
+    [alertController addAction:cancelAction];
+    [alertController addAction:sureAction];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
